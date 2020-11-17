d93a1e23d9f70416e2e178737cabab38ed506991
Felix Hartmond <felix@othrys.net>
2020-11-17 16:54:53 +0000

    add log output to post-processors

--- postprocessing/deployhook.go
+++ postprocessing/deployhook.go
@@ -3,10 +3,17 @@ package postprocessing
 import (
 	"os/exec"
 
+	log "github.com/sirupsen/logrus"
+
 	"felix-hartmond.de/projects/certbutler/common"
 )
 
 // ProcessDeployHook runs the defined deploy hook executable
 func ProcessDeployHook(config common.DeployHookConfiguration) error {
-	return exec.Command(config.Executable).Run()
+	err := exec.Command(config.Executable).Run()
+	if err != nil {
+		return err
+	}
+	log.Info("Deploy hook executed successfully")
+	return nil
 }
--- postprocessing/nginx.go
+++ postprocessing/nginx.go
@@ -2,10 +2,17 @@ package postprocessing
 
 import (
 	"os/exec"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // ProcessNginx triggers the nginx process to reload to load the new certificate
 func ProcessNginx() error {
 	//https://docs.nginx.com/nginx/admin-guide/basic-functionality/runtime-control/
-	return exec.Command("nginx", "-s", "reload").Run()
+	err := exec.Command("nginx", "-s", "reload").Run()
+	if err != nil {
+		return err
+	}
+	log.Info("Nginx reloaded successfully")
+	return nil
 }
